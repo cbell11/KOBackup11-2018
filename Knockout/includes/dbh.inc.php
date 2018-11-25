7e82c69b0f57f3e84a433becf07df2fcd60a4b96
cbell11 <chandler0522@gmail.com>
2018-11-25 13:18:33 +0700

    Add files via upload

---
+++ Knockout/includes/dbh.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+/* BlueHost Server Info
+$dbServerName = "localhost";
+$dbUsername = "knockoy5_cbell11";
+$dbPassword = "Chandler0522!";
+$dbName = "knockoy5_WPZEL";
+*/
+$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+?>
